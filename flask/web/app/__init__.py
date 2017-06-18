3da5d3d5a44e5c043477cdff8b032cd477e4bba7
Trigger <yehongjiang2012@gmail.com>
2017-06-18 22:56:32 +0800

    update

--- flask/web/app/__init__.py
+++ flask/web/app/__init__.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask
+from flask_login import LoginManager
+from flask_openid import OpenID
 from flask_sqlalchemy import SQLAlchemy
+from config import basedir
+from app import views, models
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from app import views, models
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
--- flask/web/app/models.py
+++ flask/web/app/models.py
@@ -7,11 +7,22 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return False
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
     def __repr__(self):
         return '<User %r' % (self.nickname)
 
 
-class Posts(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
@@ -19,3 +30,4 @@ class Posts(db.Model):
 
     def __repr__(self):
         return '<Post %r' % (self.body)
+
