dcc380924c9f8350c4e2ef6669bf702cf083ed2c
Trigger <yehongjiang2012@gmail.com>
2017-06-18 22:12:14 +0800

    new
    new

--- flask/web/app/models.py
+++ flask/web/app/models.py
@@ -11,11 +11,11 @@ class User(db.Model):
         return '<User %r' % (self.nickname)
 
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post %r>' % (self.body)
+        return '<Post %r' % (self.body)
